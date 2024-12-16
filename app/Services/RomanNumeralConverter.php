87792738bef4f9450c5c0d0e4ea8b2850d589a69
mick <mickb.py@gmail.com>
2024-12-16 22:13:19 +0000

    Initial commit

---
+++ app/Services/RomanNumeralConverter.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Services;
+
+class RomanNumeralConverter
+{
+
+}
+
+
+#todo
+/*
+ * use interface
+ * use REST
+ * 3 endpoints
+ * 1-3999
+ * most frequently converted
+ * last time converted
+ *
+ * Endpoints
+ * int to numeral store numeral
+ * list all "recently" converted ints
+ * list top 10 converted ints
+ * resource for the response
+ * validation not in controller
+ * clean code
+ * check pSR 12
+ * php 8.3 features used?
+ *
+ * short explanation of approach
+ *
+*/
